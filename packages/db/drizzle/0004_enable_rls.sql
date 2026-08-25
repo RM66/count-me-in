91a87d66c1b1a8be25b291fdfeb057b980eefba5
Roma Y. <rm66@ya.ru>
2026-08-25 22:59:40 +0200

    Миграция с RLS

---
+++ packages/db/drizzle/0004_enable_rls.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "public"."organizers" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "public"."services" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "public"."time_slots" ENABLE ROW LEVEL SECURITY;--> statement-breakpoint
+ALTER TABLE "public"."bookings" ENABLE ROW LEVEL SECURITY;
